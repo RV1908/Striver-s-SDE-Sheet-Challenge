9d9264094de3a93daa97332925d2f8802bdc333e
RV1908 <134372027+RV1908@users.noreply.github.com>
2023-06-07 01:32:54 +0530

    Create 014pow(x,n).cpp

---
+++ 014pow(x,n).cpp
@@ -0,0 +1,28 @@
+double myPow(double x, int n) {
+        double ans = 1;
+
+        long long nn = n;
+        if(n<0){
+            nn = abs(n);
+        }
+
+        while(nn){   
+            //even no hone se agar no ko usi no se multiply karo aur nn/2 kar do
+            if(nn%2==0){
+                x=x*x;
+                nn=nn/2;
+            }
+            //odd no h to x ko ans se multiply karo and nn ko desc karo
+            else{      
+                ans = ans*x;
+                nn = nn-1;
+            }
+        }
+
+        if(n<0) //agar negative power h to div karo
+        {
+            ans = (double)(1)/(double)(ans);
+        }
+
+        return ans;
+    }
