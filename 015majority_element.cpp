0e0554e9e552f3ffb573431019ea07fc9d314bd4
RV1908 <134372027+RV1908@users.noreply.github.com>
2023-06-07 01:53:21 +0530

    Create 015majority_element.cpp

---
+++ 015majority_element.cpp
@@ -0,0 +1,14 @@
+int majorityElement(vector<int>& nums) {
+        int ans = -1;
+        map<int,int> mp;
+        for(int i=0; i<nums.size(); i++){
+            mp[nums[i]]++;
+        }
+
+        for(auto it: mp){
+            if(it.second>(nums.size()/2)){
+                ans = it.first;
+            }
+        }
+        return ans;
+    }
